144988ade71e4b06cba4954bb3829c0eb19faa85
alencont <1185575326@qq.com>
2016-12-16 08:42:34 +0800

    init

---
+++ src/js/jquery.alert.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Administrator on 2016/12/15 0015.
+ */
+(function () {
+    function AlenaDialog(){
+
+
+
+    }
+
+    $.fn.extends({
+        AlenaDialog:AlenaDialog
+    });
+})();
+
