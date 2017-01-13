9d4c0957bf76764b35cf35ac7cceff8c75d7094d
quanlincong <1185575326@qq.com>
2017-01-13 16:02:17 +0800

    readMe 增加明确的说明文件

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # jquery.alert
-this is a easy dialog.
+一个简单的dialog插件.
+
+### 默认参数：
+    title:"",
+    content:"",
+    autoClose: false,
+    sizeType: "md" ,
+    type:"annotation"
+   
+### 可选参数
+    
+    title: ""
+    content: ""
+    height: 200p
+    width: 200px
+    
+    sizeType: sm | md | lg | auto   // 如果有sizeType大小配置，则以sizeType为准
+    type: tips | warning | error    // 不同的提示，icon，文字，会不一样
+    autoCloes: false | true         // 自动关闭
+    
+    cancel:function(){}   // 取消按钮的回调
+    certain:function(){}  // 确定按钮的回调
+    other:function(){}    // 其他按钮的回调，可自定义
+    close:function(){}    // 关闭按钮的回调
+    
--- src/js/jquery.alert.js
+++ src/js/jquery.alert.js
@@ -1,13 +1,28 @@
 /**
  * Created by Administrator on 2016/12/15 0015.
  */
-(function () {
-    function AlenaDialog(){
-
+(function ($) {
+    function Dialog(opt){
+        this.width = opt.width
+        this.height = opt.opt.height
+    }
+    
+    var classOper = {
+        rmClassName: function (dom, classname) {
+            dom.className = dom.className.replace(new RegExp("(\\s+?|^)" + classname + "(\\s+?|$)"), "");
+        },
+        addClassName: function (dom, classname) {
+            if (!this.hasClassName(dom, classname)) {
+                dom.className += " " + classname;
+            }
+        },
+        hasClassName: function (dom, classname) {
+            return new RegExp("(^|\\s+?)" + classname + "(\\s+|$)").test(dom.className);
+        }
     }
 
     $.fn.extends({
-        AlenaDialog:AlenaDialog
+        Dialog:Dialog
     });
-})();
+})(jQuery);
 
